d14e902c7d67b731ab9e0f6762e16bfc0f02eade
Jeff Palmer <jeff@Jeffs-MacBook-Pro.local>
2019-03-02 14:08:26 -0600

    Initial commit

---
+++ src/main/java/com/github/jpalmer1026/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,36 @@
+package com.github.jpalmer1026.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
+
+        // send data - asynchronous
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+    }
+}
